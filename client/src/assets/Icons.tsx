fa068fa7cba9d8bee35f54fda8e7dadb90dc5d17
RamiroPagella <ramiro.s.pagella@gmail.com>
2024-02-09 12:31:34 -0300

    icono wsp arregaldo y flecha

--- client/src/assets/Icons.tsx
+++ client/src/assets/Icons.tsx
@@ -22,7 +22,7 @@ export function LinkedinIcon(props: SVGProps<SVGSVGElement>) {
   );
 }
 
-export function ArrowDow(props: SVGProps<SVGSVGElement>) {
+export function ArrowDown (props: SVGProps<SVGSVGElement>) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
       <path
--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,11 +1,10 @@
 import style from "./home.module.scss";
-import { WhatsappIconGreen } from "../../assets/Icons";
+import { WhatsappIconGreen, ArrowDown } from "../../assets/Icons";
 
 const Home = () => {
   return (
     <div className={style.Home}>
-
-      <div className={style['title-container']}>
+      <div className={style["title-container"]}>
         <h2>- Desde 2006 -</h2>
         <hr />
         <h1>AGROFORRAJES FERREYRA </h1>
@@ -13,7 +12,17 @@ const Home = () => {
         <h2>Calidad Asegurada</h2>
       </div>
 
-      <WhatsappIconGreen className={style['whatsapp-icon']} />
+      <a
+        className={style["whatsapp-container"]}
+        href="https://wa.me/3329561698"
+        target="_blank"
+      >
+        <WhatsappIconGreen className={style.icon} />
+      </a>
+
+      <a className={style['arrow-container']}>
+        <ArrowDown className={style.icon} />
+      </a>
     </div>
   );
 };
