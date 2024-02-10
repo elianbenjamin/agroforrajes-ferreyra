a547009139ff7756a36be5d309c4f79e4874dfee
elianbenjamin <elianbenjamin30@gmail.com>
2024-02-10 11:06:49 -0300

    css Service

--- client/src/views/Service/Service.tsx
+++ client/src/views/Service/Service.tsx
@@ -1,17 +1,44 @@
 "use client";
-import React from 'react';
-import styles from './Service.module.scss';
+import React from "react";
+import styles from "./Service.module.scss";
 
 export type ServiceProps = {
-	// types...
-}
+  // types...
+};
 
-const Service: React.FC<ServiceProps>  = ({}) => {
-	return (
-		<div className={styles.Service} id='service'>
- 			Service works!
- 		</div>
-	);
+const Service: React.FC<ServiceProps> = ({}) => {
+  return (
+    <div className={styles.Service} id="service">
+      <div className={styles["titulo-container"]}>
+        <h1 className={styles.titulo}>Nuestros Servicios</h1>
+      </div>
+
+      <div className={styles["container-servicios"]}>
+        <div className={styles["container-picado"]}>
+          <h2 className={styles.picado}>
+            Picado de FORRAJES <br />
+            <p className={styles["text-picado"]}>
+              Especialización en la confección de comida de calidad para tambos
+              y feedlots. Ensilado de maíz, sorgo, cereales de invierno y
+              forrajes de primavera, en silo bolsa, banker, torta y autconsumo
+            </p>
+          </h2>
+        </div>
+
+        <div className={styles["container-siembra"]}>
+          <h2 className={styles.siembra}>
+            Siembra de PRECISION <br />
+            <p className={styles["text-siembra"]}>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Molestiae consequatur eius ipsa qui reprehenderit ipsum, fuga id
+              illo repellendus consectetur doloremque mollitia obcaecati!
+              Tenetur voluptas quae molestias suscipit modi tempore.
+            </p>
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Service;
