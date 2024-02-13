c8a1ff252371e22fd0108770cd00e0ab26d9e01c
RamiroPagella <ramiro.s.pagella@gmail.com>
2024-02-13 18:13:00 -0300

    footer terminado

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,15 +1,27 @@
+import styles from "./footer.module.scss";
 import {
   InstagramIcon,
   LinkedinIcon,
   MessageIcon,
   WhatsappIconDark,
 } from "../../assets/Icons";
-import styles from "./footer.module.scss";
+import googleMapsScreenshot from "../../assets/images/googleMaps-screenshot.png";
+import { LocationIcon } from "../../assets/Icons";
+
 
 const Footer = () => {
   return (
     <div className={styles.Footer}>
-      <section className={styles.container1}></section>
+      <section className={styles.container1}>
+        <p>
+          <LocationIcon />
+          Justo Daract, San Luis.
+        </p>
+        <a href={"https://maps.app.goo.gl/aKBcVWJVtjAncqq59"} target="_blank">
+          <img src={googleMapsScreenshot} />
+        </a>
+      </section>
+
       <section className={styles.container2}>
         <div className={styles["title-container"]}>
           <h1 className={styles.title}>Mantengámonos en contacto</h1>
@@ -33,6 +45,7 @@ const Footer = () => {
 
         <p>Copyright © 2006-2024 Agroforrajes Ferreyra S.R.L</p>
       </section>
+
       <section className={styles.container3}>
         <h1 className={styles["contact-title"]}>Contactos:</h1>
         <p className={styles["contact-item"]}>
@@ -41,7 +54,9 @@ const Footer = () => {
         <p className={styles["contact-item"]}>
           Carlos Ferreyra tel: 3328732467
         </p>
-        <p className={styles["contact-item"]}>Adrian Ferreyra tel: 3328732467</p>
+        <p className={styles["contact-item"]}>
+          Adrian Ferreyra tel: 3328732467
+        </p>
         <p className={styles["contact-item"]}>
           Fernando Ferreyra tel: 3328732467
         </p>
