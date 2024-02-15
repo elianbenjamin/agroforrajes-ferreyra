4efa35a901369b66a1e8b85baf79e6c567f01377
RamiroPagella <ramiro.s.pagella@gmail.com>
2024-02-15 14:57:52 -0300

    error page

--- client/src/components/ErrorPage/ErrorPage.tsx
+++ client/src/components/ErrorPage/ErrorPage.tsx
@@ -1,16 +1,22 @@
-import { Carousel } from '../Carousel';
-import styles from './errorPage.module.scss'
-
+import { NavLink } from "react-router-dom";
+import { Carousel } from "../Carousel";
+import styles from "./errorPage.module.scss";
 
 const ErrorPage = () => {
-    return (
-        <div className={styles['error-page']}>
-            <h1>404</h1>
-            <p>Pagina no encontrada</p>
-            <button>Volver al Home</button>
-            <Carousel />
-        </div>
-    )
-}
+  return (
+    <div className={styles["error-page"]}>
+      <div className={styles["container"]}>
 
-export default  ErrorPage;
+        <h2>- Error 404 -</h2>
+        <hr />
+        <h1> PAGINA NO ENCONTRADA </h1>
+        <hr />
+        <NavLink to={'/'}>Volver al inicio</NavLink>
+      </div>
+
+      <Carousel />
+    </div>
+  );
+};
+
+export default ErrorPage;
